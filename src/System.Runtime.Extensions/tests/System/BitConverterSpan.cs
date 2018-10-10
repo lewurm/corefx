74863410acb04654babbb5e3b4d33b600832eca1
Bernhard Urban <lewurm@gmail.com>
2018-10-10 07:56:32 -0400

    Revert "Ignore ConvertFromFloat and ConvertFromDouble on mono"
    This reverts commit 260cfea512e56d1edf144ea9f533fd0cd1ba6cb8.

--- src/System.Runtime.Extensions/tests/System/BitConverterSpan.cs
+++ src/System.Runtime.Extensions/tests/System/BitConverterSpan.cs
@@ -96,10 +96,6 @@ namespace System.Tests
 
         public override void ConvertFromFloat(float num, byte[] expected)
         {
-#if MONO // https://github.com/mono/mono/issues/10963
-            if (float.IsNaN(num))
-                return;
-#endif
             Span<byte> span = new Span<byte>(new byte[4]);
             Assert.True(BitConverter.TryWriteBytes(span, num));
             Assert.Equal(expected, span.ToArray());
@@ -107,10 +103,6 @@ namespace System.Tests
 
         public override void ConvertFromDouble(double num, byte[] expected)
         {
-#if MONO // https://github.com/mono/mono/issues/10963
-            if (double.IsNaN(num))
-                return;
-#endif
             Span<byte> span = new Span<byte>(new byte[8]);
             Assert.True(BitConverter.TryWriteBytes(span, num));
             Assert.Equal(expected, span.ToArray());
